b908589df0e1a86f7a48df201c8d8a317ef47a0a
ahmedjenhani <ahmed.jenhani2@gmail.com>
2024-04-26 15:25:19 +0200

    second commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * 
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * 
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('isAdmin', function ($user) {
+            return $user->isAdmin === 1;
+        });
+        Gate::define('isSeller', function ($user) {
+            return $user->isAdmin === 0;
+        });
+
+        //
+    }
+}
